9210d7d78a35808aa9b3e439b20d98258c386a97
Divyansh Chouhan <118560135+divyansh2222@users.noreply.github.com>
2025-02-06 15:21:29 +0530

    dddfd

--- components/Layout.js
+++ components/Layout.js
@@ -2,30 +2,18 @@
 
 import NavBar from "./Navbar";
 import Sidebar from "./Sidebar";
-import { useState } from "react";
 
 const Layout = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
 
   return (
-    <div className="flex  h-screen">
+    <div className="flex h-screen">
       {/* Sidebar */}
-      <div
-        className={`transition-all duration-300 ${
-          isSidebarOpen ? "w-64" : "w-20"
-        }`}
-      >
-        <Sidebar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
-      </div>
+      <Sidebar />
 
       {/* Main Content Area */}
-      <div className={`flex ${isSidebarOpen ? "w-100" : "w-20"} flex-col  flex-1`}>
+      <div className="flex flex-col flex-1">
         {/* Navbar */}
-        <NavBar />
+        <NavBar  />  
 
         {/* Page Content */}
         <main className="flex-1 overflow-y-auto dark:text-white dark:bg-darkbackground p-6">

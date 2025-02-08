67d4aa67f4b2f1c5002f09b4323dfcd179020ef1
Divyansh Chouhan <118560135+divyansh2222@users.noreply.github.com>
2025-02-08 17:57:23 +0530

    ghgsdah

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -14,7 +14,7 @@ const page = ({session,signOut}) => {
       <Box/>
     <div className='flex flex-row items-center p-4 rounded-xl border-2 gap-10 sm:flex-col md:flex-row lg:flex-row '> 
      
-    <div className="hidden xxl:block">
+    <div className="hidden lg:block">
   <Barchart />
 </div>
 
